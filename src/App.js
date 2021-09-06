58f99ec921c9bd52d5928fecb0406acf520f9757
Deep Patel <dmpate@deakin.edu.au>
2021-09-06 18:22:21 +1000

    Container for weather added Date function added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+const {format} = require('date-fns');
 
 const api = {
   key: "fc2c0a430753d1df77c3ce68f67540a1",
@@ -8,6 +8,10 @@ const api = {
 }
 
 function App() {
+  const date = new Date();
+  const time = format(date, 'HH:mm');
+  const today = format(date, 'MMMM do, yyyy, EEEE');
+
     return (
       <div className="app">
         <main>
@@ -18,9 +22,16 @@ function App() {
             placeholder="Search..."
             />
           </div>
-          <div className="location-box">
-            <div className="location">New South Whales, AU</div>
-            <div className="date"></div>
+          <div className="date">{today}</div>
+          <div className="location-container">
+            <div className="location-box">
+              <div className="location">New South Whales, AU</div>
+              <div className="time">{time}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp"> 15°C </div>
+              <div className="weather"> Sunny </div>
+            </div>
           </div>
         </main>
       </div>
