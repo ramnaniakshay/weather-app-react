d7807641e018a3042775fef2124a6c5a5f88d991
Deep Patel <dmpate@deakin.edu.au>
2021-09-08 14:48:19 +1000

    citiesList to for location data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
+import api from './api';
 const {format} = require('date-fns');
-
-const api = {
-  key: "fc2c0a430753d1df77c3ce68f67540a1",
-  base: "https://api.openweathermap.org/data/2.5/"
-
-}
 
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
-    if (evt.key === "Enter"){
+  const search = e => {
+    if (e.key === "Enter"){
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
       .then(result => {
@@ -33,7 +28,7 @@ function App() {
             <input
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Search for city..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
@@ -57,6 +52,7 @@ function App() {
             </div>
           ) :
           // /* TODO: Add different cities to the list of weather here */
+          // Sydney, Melbourne, Brisbane, Perth, Adelaide, Hobart, Canberra, Darwin
           ('')}
         </main>
       </div>
---
+++ src/api.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const api = {
+    key: "fc2c0a430753d1df77c3ce68f67540a1",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+export default api;
